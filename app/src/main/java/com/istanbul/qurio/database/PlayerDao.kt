05a96a834d07c05a274a25d08f8badfd8035639e
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-10-17 07:35:11 +0300

    Add PlayerDao interface for player life and coins management

---
+++ app/src/main/java/com/istanbul/qurio/database/PlayerDao.kt
@@ -0,0 +1,23 @@
+package com.istanbul.qurio.database
+
+import androidx.room.*
+import com.istanbul.qurio.model.Player
+
+@Dao
+interface PlayerDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun savePlayer(player: Player)
+
+    @Query("SELECT * FROM player LIMIT 1")
+    suspend fun getPlayer(): Player?
+
+    @Query("UPDATE player SET lives = lives + 1, coins = 200 - :coins")
+    suspend fun buyLife(coins: Int)
+
+    @Query("UPDATE player SET lives = lives - 1 WHERE lives > 0")
+    suspend fun loseLife()
+
+    @Query("SELECT lives FROM player LIMIT 1")
+    suspend fun getNumberOfLife(): Int
+}
